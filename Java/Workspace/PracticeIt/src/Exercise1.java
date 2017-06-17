54c0f4480bb2f855b16634106236dac97da3a1e1
Dondee Tan <dondee.tan@alaskaair.com>
2017-06-16 20:54:32 -0700

    Adding Java Files

---
+++ Java/Workspace/PracticeIt/src/Exercise1.java
@@ -0,0 +1,18 @@
+public class Exercise1
+{
+	public static void main(String[] args)
+	{	
+		//A "quoted" String is
+		System.out.println("A \"quoted\" String is");
+		//'much' better if you learn
+		System.out.println("'much' better if you learn");
+		//the rules of "escape sequences."
+		System.out.println("the rules of \"escape sequences.\"");
+		//Also, "" represents an empty String.
+		System.out.println("Also, \"\" represents an empty String.");
+		//Don't forget: use \" instead of " !
+		System.out.println("Don't forget: use \\\" instead of \" !");
+		//'' is not the same as "
+		System.out.println("'' is not the same as \"");
+	}
+}
