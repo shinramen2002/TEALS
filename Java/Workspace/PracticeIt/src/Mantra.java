dd8ea0aa6f714e58221f8882f95afc7dffb86a45
Dondee <Tan>
2017-06-17 06:13:30 -0700

    Adding 2 new Project files

---
+++ Java/Workspace/PracticeIt/src/Mantra.java
@@ -0,0 +1,14 @@
+
+public class Mantra {
+
+	public static void main(String[] args) {
+		printParagraph();
+		System.out.println();
+		printParagraph();
+	}
+	
+	private static void printParagraph(){
+		System.out.println("There's one thing every coder must understand:");
+		System.out.println("The System.out.println command.");
+	}
+}
---
+++ Java/Workspace/PracticeIt/src/Stewie2.java
@@ -0,0 +1,16 @@
+
+public class Stewie2 {
+
+	public static void main(String[] args) {
+		System.out.println("//////////////////////");
+		printParagraph();
+	}
+
+	private static void printParagraph(){
+		for(int i = 1; i <= 5; i++ )
+		{
+			System.out.println("|| Victory is mine! ||");
+			System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");	
+		}		
+	}
+}
