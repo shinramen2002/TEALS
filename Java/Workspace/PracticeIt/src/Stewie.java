86e6fe09cc093a3b568dc286a4bdd046dcc1fd96
Dondee <Tan>
2017-06-17 06:41:35 -0700

    Added new files (Homework)

---
+++ Java/Workspace/PracticeIt/src/Stewie.java
@@ -0,0 +1,16 @@
+
+public class Stewie {
+
+	public static void main(String[] args) {
+		System.out.println("//////////////////////");
+		printParagraph();
+	}
+
+	private static void printParagraph(){
+		for(int i = 1; i <= 1; i++ )
+		{
+			System.out.println("|| Victory is mine! ||");
+			System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");	
+		}		
+	}
+}
---
+++ Java/Workspace/PracticeIt/src/WellFormed.java
@@ -0,0 +1,23 @@
+public class WellFormed {//
+
+	public static void main(String[] args) {
+//		A well-formed Java program has
+		System.out.println("A well-formed Java program has");
+//		a main method with { and }
+		System.out.println("a main method with { and }");
+//		braces.
+		System.out.println("braces.");
+		System.out.println();
+//
+//		A System.out.println statement
+		System.out.println("A System.out.println statement");
+//		has ( and ) and usually a
+		System.out.println("has ( and ) and usually a");
+//		String that starts and ends
+		System.out.println("String that starts and ends");
+//		with a " character.
+		System.out.println("with a \" character.");
+//		(But we type \" instead!)
+		System.out.println("(But we type \\\" instead!)");
+	}
+}
