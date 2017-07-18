7434016bbcfa61b12664364c64fe39a45163eebb
Dondee Tan <dondee.tan@alaskaair.com>
2017-07-17 21:21:15 -0700

    Refactored code and Unit Tests

--- Java/Workspace/FracCalc/src/TestFiles/FracCalcProduceAnswerTests.java
+++ Java/Workspace/FracCalc/src/TestFiles/FracCalcProduceAnswerTests.java
@@ -1,240 +1,124 @@
 package TestFiles;
 
-
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
-import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
-
 import fracCalc.FracCalc;
 import fracCalc.FracCalcTestHelper;
 
-
-
 public class FracCalcProduceAnswerTests {
-	   @Test public void ValidateInput_ShouldBeCorrectFormat() 
-	    {
-		  assertEquals("4", FracCalc.produceAnswer("2 + 4"));		   
-	    }
-	   
-	   @Test public void ValidateInput_ShouldNotBeCorrectFormat() 
-	    {
-		  assertEquals("Input is not in the correct format","Input Expression is not in the correct Format. Values and operators will be separated by a single space", FracCalc.produceAnswer("2 +"));		   
-	    }
-	   
-	   @Test public void ValidateInput_ShouldBeCorrectFormatUsingFractions() 
-	    {
-		  assertEquals("4", FracCalc.produceAnswer("3_1/2 + 1/4"));		   
-	    }
-	   
-	   @Test public void ValidateInput_ShouldBeCorrectFormatUsingFractionsReturnsOperand2() 
-	    {
-		  assertEquals("4", FracCalc.produceAnswer("3_1/2 + 3_1/4"));		   
-	    }
-	   
-	   // ParseFraction Tests
-	   @Test public void Validate_ParseFraction_Mixed() 
-	    {
-		   String fraction = "6_5/8";   
-		   assertEquals("Incorrect Fraction Parsing","whole:6 numerator:5 denominator:8", FracCalc.parseFractionCheckPoint2(fraction));
-		   System.out.println(FracCalc.parseFractionCheckPoint2(fraction));   
-	    }
-	   
-	   // ParseFraction Tests
-	   @Test public void Validate_ParseFraction_Improper() 
-	    {
-		   String fraction = "5/8";   
-		   assertEquals("Incorrect Fraction Parsing","whole:0 numerator:5 denominator:8", FracCalc.parseFractionCheckPoint2(fraction));
-		   System.out.println(FracCalc.parseFractionCheckPoint2(fraction));   
-	    }
-	   
-	   @Test public void Validate_ParseFraction_OnlyWholeNumber() 
-	    {
-		   String fraction = "1";   
-		   assertEquals("Incorrect Fraction Parsing","whole:1 numerator:0 denominator:1", FracCalc.parseFractionCheckPoint2(fraction));
-		   System.out.println(FracCalc.parseFractionCheckPoint2(fraction));   
-	    }
-	   
-	   
-	   //TurnImproperFractionToMixedFraction Tests
-	   @Test public void Validate_StaticVariablesSet()
-	    {
-	     	FracCalc.parseFraction("1/4", true);	
-	     	FracCalc.parseFraction("1_3/4", false);
-	     	 assertEquals(4,FracCalc.operand2Denominator);
-	     	 assertEquals(3, FracCalc.operand2Numerator);
-	    }
-	   
-	   //Find GCD
-	   @Test public void Validate_findGcd()
-	    {
-		   int result = FracCalc.findGcd(54, 24); 
-		   assertEquals(6, result);
-		}
-	   
-	   //Convert Mixed to Improper Fraction
-	   @Test public void Validate_ConvertToImproper()
-	    {
-		   FracCalc.parseFraction("1_3/4", false);
-		   assertEquals(7, FracCalc.operand2ImproperNumerator);
-		}
-	  
-	   
-	 //Calculation Tests
-	   //Test Case 1: 1/4 + 1_1/2 =	1_3/4
-	   @Test public void Validate_CalculateAddSameDenominator()
-	    {
-		   FracCalc.parseFraction("3/4", true);
-		   FracCalc.parseFraction("2/4", false);
-		   String result = "";
-		   if (FracCalc.operand1Denominator == FracCalc.operand2Denominator)
-		   {
-			   int tempnumerator = FracCalc.operand1Numerator + FracCalc.operand2Numerator;
-			   if (tempnumerator % FracCalc.operand1Denominator == 0 )
-			   {
-				   result = Integer.toString(tempnumerator / FracCalc.operand1Denominator);
-			   }
-			   else
-			   {
-				   result = Integer.toString(tempnumerator) + "/" + Integer.toString(FracCalc.operand1Denominator);
-			   }
-		   }
-		   assertEquals("5/4", result);
-		}
-	   
-	   @Test public void Validate_CalculateAddSameDenominatorResultsToWhole()
-	    {
-		   FracCalc.parseFraction("3/4", true);
-		   FracCalc.parseFraction("1/4", false);
-		   String result = "";
-		   int tempnumerator = 0;
-		   int commondenominator = 0;
-		   if (FracCalc.operand1Denominator == FracCalc.operand2Denominator)
-		   {
-			   tempnumerator = FracCalc.operand1Numerator + FracCalc.operand2Numerator;
-			   if (tempnumerator % FracCalc.operand1Denominator == 0 )
-			   {
-				   result = Integer.toString(tempnumerator / FracCalc.operand1Denominator);
-			   }
-			   else
-			   {
-				   result = Integer.toString(tempnumerator) + "/" + Integer.toString(FracCalc.operand1Denominator);
-			   }
-		   }
-		   else
-		   {
-			   commondenominator = FracCalc.operand1Denominator * FracCalc.operand2Denominator;
-			   FracCalc.operand1ImproperNumerator = FracCalc.operand1ImproperNumerator * (commondenominator /  FracCalc.operand1Denominator);
-			   FracCalc.operand2ImproperNumerator = FracCalc.operand2ImproperNumerator * (commondenominator /  FracCalc.operand2Denominator);
-			   //if (FracCalc.operand1Denominator == FracCalc.operand2Denominator)
-			   //{
-				   tempnumerator = FracCalc.operand1ImproperNumerator + FracCalc.operand2ImproperNumerator;
-				   if (tempnumerator % commondenominator == 0 )
-				   {
-					   result = Integer.toString(tempnumerator / commondenominator);
-				   }
-				   else
-				   {
-					   result = Integer.toString(tempnumerator) + "/" + Integer.toString(commondenominator);
-				   }
+	@Test
+	public void ValidateInput_ShouldBeCorrectFormat() {
+		assertEquals("4", FracCalc.produceAnswer("2 + 4"));
+	}
 
-				   int gcd = FracCalc.findGcd(tempnumerator, commondenominator);
-				   result = Integer.toString(tempnumerator / gcd) + "/" + Integer.toString(commondenominator / gcd);
-		   }
-		   assertEquals("1", result);
-		}
-	   
-	   @Test public void Validate_CalculateAddDifferentDenominator()
-	    {
-		   FracCalc.parseFraction("3/4", true);
-		   FracCalc.parseFraction("1/3", false);
-		   String result = "";
-		   int tempnumerator = 0;
-		   int commondenominator = 0;
-		   if (FracCalc.operand1Denominator == FracCalc.operand2Denominator)
-		   {
-			   tempnumerator = FracCalc.operand1Numerator + FracCalc.operand2Numerator;
-			   if (tempnumerator % FracCalc.operand1Denominator == 0 )
-			   {
-				   result = Integer.toString(tempnumerator / FracCalc.operand1Denominator);
-			   }
-			   else
-			   {
-				   result = Integer.toString(tempnumerator) + "/" + Integer.toString(FracCalc.operand1Denominator);
-			   }
-		   }
-		   else
-		   {
-			   commondenominator = FracCalc.operand1Denominator * FracCalc.operand2Denominator;
-			   FracCalc.operand1ImproperNumerator = FracCalc.operand1ImproperNumerator * (commondenominator /  FracCalc.operand1Denominator);
-			   FracCalc.operand2ImproperNumerator = FracCalc.operand2ImproperNumerator * (commondenominator /  FracCalc.operand2Denominator);
-			   //if (FracCalc.operand1Denominator == FracCalc.operand2Denominator)
-			   //{
-				   tempnumerator = FracCalc.operand1ImproperNumerator + FracCalc.operand2ImproperNumerator;
-				   if (tempnumerator % commondenominator == 0 )
-				   {
-					   result = Integer.toString(tempnumerator / commondenominator);
-				   }
-				   else
-				   {
-					   result = Integer.toString(tempnumerator) + "/" + Integer.toString(commondenominator);
-				   }
+	@Test
+	public void ValidateInput_ShouldNotBeCorrectFormat() {
+		assertEquals("Input is not in the correct format",
+				"Input Expression is not in the correct Format. Values and operators will be separated by a single space",
+				FracCalc.produceAnswer("2 +"));
+	}
 
-				   int gcd = FracCalc.findGcd(tempnumerator, commondenominator);
-				   result = Integer.toString(tempnumerator / gcd) + "/" + Integer.toString(commondenominator / gcd);
-		   }		   
-		   assertEquals("13/12", result);
-		}
-	   
-	   //Calculation Tests
-	   //Test Case 1: 1/4 + 1_1/2 =	1_3/4
-	   @Test public void Validate_CalculateAddTestCase1()
-	    {
-		   FracCalc.parseFraction("1/4", true);
-		   FracCalc.parseFraction("1_1/2", false);
-		   String result = "";
-		   int tempnumerator = 0;
-		   int commondenominator = 0;
-		   if (FracCalc.operand1Denominator == FracCalc.operand2Denominator)
-		   {
-			   tempnumerator = FracCalc.operand1Numerator + FracCalc.operand2Numerator;
-			   if (tempnumerator % FracCalc.operand1Denominator == 0 )
-			   {
-				   result = Integer.toString(tempnumerator / FracCalc.operand1Denominator);
-			   }
-			   else
-			   {
-				   result = Integer.toString(tempnumerator) + "/" + Integer.toString(FracCalc.operand1Denominator);
-			   }
-		   }
-		   else
-		   {
-			   commondenominator = FracCalc.operand1Denominator * FracCalc.operand2Denominator;
-			   FracCalc.operand1ImproperNumerator = FracCalc.operand1ImproperNumerator * (commondenominator /  FracCalc.operand1Denominator);
-			   FracCalc.operand2ImproperNumerator = FracCalc.operand2ImproperNumerator * (commondenominator /  FracCalc.operand2Denominator);
-			   //if (FracCalc.operand1Denominator == FracCalc.operand2Denominator)
-			   //{
-				   tempnumerator = FracCalc.operand1ImproperNumerator + FracCalc.operand2ImproperNumerator;
-				   if (tempnumerator % commondenominator == 0 )
-				   {
-					   result = Integer.toString(tempnumerator / commondenominator);
-				   }
-				   else
-				   {
-					   result = Integer.toString(tempnumerator) + "/" + Integer.toString(commondenominator);
-				   }
+	@Test
+	public void ValidateInput_ShouldBeCorrectFormatUsingFractions() {
+		assertEquals("4", FracCalc.produceAnswer("3_1/2 + 1/4"));
+	}
 
-				   int gcd = FracCalc.findGcd(tempnumerator, commondenominator);
-				   result = Integer.toString(tempnumerator / gcd) + "/" + Integer.toString(commondenominator / gcd);
-		   }	
-		   assertEquals("7/4", result);
-		}
+	@Test
+	public void ValidateInput_ShouldBeCorrectFormatUsingFractionsReturnsOperand2() {
+		assertEquals("4", FracCalc.produceAnswer("3_1/2 + 3_1/4"));
+	}
 
-	   //Test Case 2: 8/4 + 2	= 4
-	   //Test Case 3: -1 * -1/2 = 1/2
-	   //Test Case 4: -11/17 + -1/17 = -12/17
-	   //Test Case 5: 0 * 25_462/543	0
+	// ParseFraction Tests
+	@Test
+	public void Validate_ParseFraction_Mixed() {
+		String fraction = "6_5/8";
+		assertEquals("Incorrect Fraction Parsing", "whole:6 numerator:5 denominator:8",
+				FracCalc.parseFractionCheckPoint2(fraction));
+		System.out.println(FracCalc.parseFractionCheckPoint2(fraction));
+	}
+
+	// ParseFraction Tests
+	@Test
+	public void Validate_ParseFraction_Improper() {
+		String fraction = "5/8";
+		assertEquals("Incorrect Fraction Parsing", "whole:0 numerator:5 denominator:8",
+				FracCalc.parseFractionCheckPoint2(fraction));
+		System.out.println(FracCalc.parseFractionCheckPoint2(fraction));
+	}
+
+	@Test
+	public void Validate_ParseFraction_OnlyWholeNumber() {
+		String fraction = "1";
+		assertEquals("Incorrect Fraction Parsing", "whole:1 numerator:0 denominator:1",
+				FracCalc.parseFractionCheckPoint2(fraction));
+		System.out.println(FracCalc.parseFractionCheckPoint2(fraction));
+	}
+
+	// TurnImproperFractionToMixedFraction Tests
+	@Test
+	public void Validate_StaticVariablesSet() {
+		FracCalc.parseFraction("1/4", true);
+		FracCalc.parseFraction("1_3/4", false);
+		assertEquals(4, FracCalc.operand2Denominator);
+		assertEquals(3, FracCalc.operand2Numerator);
+	}
+
+	// Find GCD
+	@Test
+	public void Validate_findGcd() {
+		int result = FracCalc.findGcd(54, 24);
+		assertEquals(6, result);
+	}
+
+	// Convert Mixed to Improper Fraction
+	@Test
+	public void Validate_ConvertToImproperNumerator() {
+		FracCalc.parseFraction("1_3/4", false);
+		assertEquals(7, FracCalc.operand2ImproperNumerator);
+	}
+
+	// Calculation Tests
+	// Test Case 1: 1/4 + 1_1/2 = 1_3/4
+	@Test
+	public void Validate_CalculateAddSameDenominator() {
+		FracCalc.parseFraction("3/4", true);
+		FracCalc.parseFraction("2/4", false);
+		String result = FracCalc.Add();
+		assertEquals("5/4", result);
+	}
+
+	@Test
+	public void Validate_CalculateAddSameDenominatorResultsToWhole() {
+		FracCalc.parseFraction("3/4", true);
+		FracCalc.parseFraction("1/4", false);
+		String result = FracCalc.Add();
+		assertEquals("1", result);
+	}
+
+	@Test
+	public void Validate_CalculateAddDifferentDenominator() {
+		FracCalc.parseFraction("3/4", true);
+		FracCalc.parseFraction("1/3", false);
+		String result = FracCalc.Add();
+		assertEquals("13/12", result);
+	}
+
+	// Calculation Tests
+	// Test Case 1: 1/4 + 1_1/2 = 1_3/4
+	@Test
+	public void Validate_CalculateAddTestCase1() {
+		FracCalc.parseFraction("1/4", true);
+		FracCalc.parseFraction("1_1/2", false);
+		String result = FracCalc.Add();
+		assertEquals("7/4", result);
+	}
+
+	// Test Case 2: 8/4 + 2 = 4
+	// Test Case 3: -1 * -1/2 = 1/2
+	// Test Case 4: -11/17 + -1/17 = -12/17
+	// Test Case 5: 0 * 25_462/543 0
 }
--- Java/Workspace/FracCalc/src/fracCalc/FracCalc.java
+++ Java/Workspace/FracCalc/src/fracCalc/FracCalc.java
@@ -4,195 +4,209 @@ import java.util.Scanner;
 
 public class FracCalc {
 
-	//Operand 1
+	// Operand 1
 	public static int operand1Wholenumber = 0;
 	public static int operand1Numerator = 0;
 	public static int operand1Denominator = 0;
 	public static int operand1ImproperNumerator = 0;
-	
-	//Operand 2
+
+	// Operand 2
 	public static int operand2Wholenumber = 0;
 	public static int operand2Numerator = 0;
 	public static int operand2Denominator = 0;
 	public static int operand2ImproperNumerator = 0;
-	
-	//Operator
+
+	// Operator
 	public static String operator = "";
-	
-    public static void main(String[] args) 
-    {
-    	Scanner console = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		Scanner console = new Scanner(System.in);
 		System.out.print("Please enter your equation (quit to quit): ");
 		String equation = console.nextLine();
-	
 
-		while (!equation.toLowerCase().equals("quit"))
-		{
+		while (!equation.toLowerCase().equals("quit")) {
 			String results = produceAnswer(equation);
-			//Checkpoint 1: Parsing one line of input
+			// Checkpoint 1: Parsing one line of input
 			System.out.println(results);
 			System.out.print("Please enter your equation (quit to quit): ");
-			equation = console.nextLine();			
+			equation = console.nextLine();
 		}
 		console.close();
 		System.out.print("Fraction Calculator is Closed!");
-    }
-    
-    // ** IMPORTANT ** DO NOT DELETE THIS FUNCTION.  This function will be used to test your code
-    // This function takes a String 'input' and produces the result
-    //
-    // input is a fraction string that needs to be evaluated.  For your program, this will be the user input.
-    //      e.g. input ==> "1/2 + 3/4"
-    //        
-    // The function should return the result of the fraction after it has been calculated
-    //      e.g. return ==> "1_1/4"
-    public static String produceAnswer(String input)
-    { 
-        // Requirement 1
-    	//Input will be in the form of a value, followed by an arithmetic operator, and then another value. Values and operators will be separated by a single space. Values will contain no spaces.
-    	String[] arrayinput = input.split(" ");    	
-        if (arrayinput.length != 3)
-        {
-        	return "Input Expression is not in the correct Format. Values and operators will be separated by a single space";
-        }
-        else
-        {
-        	String operand1 = arrayinput[0];
-        	String operator = arrayinput[1];
-        	String operand2 = arrayinput[2];
-        	//Input values may be in the form of
-        	//mixed fractions 1_3/4
-        	//proper fractions 2/5
-        	//improper fractions  8/4 
-        	//integers. -1 
-        	//The integer and fraction parts of a mixed fraction will be separated by an underscore (_) (e.g., “1_3/4” is one and three fourth to distinguish it from “13/4” which is thirteen fourth).
-        	parseFraction(operand1, true);	
-        	parseFraction(operand2, false);	
-        	FracCalc.operator = operator;
-        	return "4";
-        }        
-    }
-
-    // TODO: Fill in the space below with any helper methods that you think you will need
-    
-    public static void parseFraction(String operand, Boolean operand1) {
-    	   String fraction = operand;   
-		   String wholenumber = "0";
-		   String numerator = "0";
-		   String denominator = "0";
-		   String[] splitwholenumber = fraction.split("_");
-		   if (splitwholenumber.length == 2)
-		   {
-			   //Mixed Fraction Condition
-			   wholenumber = fraction.split("_")[0];
-			   numerator = fraction.split("_")[1].split("/")[0];
-			   denominator = fraction.split("_")[1].split("/")[1];
-		   }
-		   else
-		   {
-			   //Check if it's whole number only
-			   String[] splitnumeratoranddenominator = fraction.split("/");
-			   //If there are 2 values in array, this means it's a fraction else it's only a whole number
-			   if (splitnumeratoranddenominator.length == 2)
-			   {
-				   numerator = splitnumeratoranddenominator[0];
-				   denominator = splitnumeratoranddenominator[1];				   
-			   }
-			   else
-			   {
-				   wholenumber = operand;
-				   denominator = "1";
-			   }
-		   }
-		   
- 		   SetStaticFieldValues(operand1, Integer.parseInt(wholenumber), Integer.parseInt(numerator), Integer.parseInt(denominator));
- 		   
-    }
-    
-    public static void SetStaticFieldValues(Boolean operand1, int wholenumber, int numerator, int denominator) {
-    	
-    	if (operand1) { 		   
- 		   FracCalc.operand1Wholenumber = wholenumber;
- 		   FracCalc.operand1Numerator = numerator;
- 		   FracCalc.operand1Denominator  = denominator;
- 		   if (wholenumber != 0)
- 		   {
- 			  FracCalc.operand1ImproperNumerator = (wholenumber * denominator) + numerator;   
- 		   }
- 		   else
- 		   {
- 			  FracCalc.operand1ImproperNumerator = numerator;
- 		   }
- 		   
-    	}
-    	else
-    	{
-    	   FracCalc.operand2Wholenumber = wholenumber;
-   		   FracCalc.operand2Numerator = numerator;
-   		   FracCalc.operand2Denominator  = denominator;
-   		   if (wholenumber != 0)
- 		   {
-   			  FracCalc.operand2ImproperNumerator = (wholenumber * denominator) + numerator;  
- 		   }
-   		 else
-		   {
-			  FracCalc.operand2ImproperNumerator = numerator;
-		   }
-    	}
-    }
-    
-	   public static int findGcd(int number1, int number2)
-	   { 
-		   //In Euclid's algorithm, we start with two numbers X and Y. If Y is zero then greatest common divisor of both will be X, but if Y is not zero then we assign the Y to X and Y becomes X%Y. Once again we check if Y is zero, if yes then we have our greatest common divisor or GCD otherwise we keep continue like this until Y becomes zero. Since we are using modulo operator, the number is getting smaller and smaller at each iteration, so the X%Y will eventually become zero.
-		   if(number2 == 0)
-		   { 
-			   return number1; 
-		   }
-		   return findGcd(number2, number1 % number2);
 	}
-	   
-	   public static void Add() {
-	   }
-    
-    public static String parseFractionCheckPoint2(String operand) {
- 	   String fraction = operand;   
-		   String wholenumber = "0";
-		   String numerator = "0";
-		   String denominator = "0";
-		   String[] splitwholenumber = fraction.split("_");
-		   if (splitwholenumber.length == 2)
-		   {
-			   //Mixed Fraction Condition
-			   wholenumber = fraction.split("_")[0];
-			   numerator = fraction.split("_")[1].split("/")[0];
-			   denominator = fraction.split("_")[1].split("/")[1];
-		   }
-		   else
-		   {
-			   //Check if it's whole number only
-			   String[] splitnumeratoranddenominator = fraction.split("/");
-			   //If there are 2 values in array, this means it's a fraction else it's only a whole number
-			   if (splitnumeratoranddenominator.length == 2)
-			   {
-				   numerator = splitnumeratoranddenominator[0];
-				   denominator = splitnumeratoranddenominator[1];				   
-			   }
-			   else
-			   {
-				   wholenumber = operand;
-				   denominator = "1";
-			   }
-		   }	   
-		  
-		   return "whole:" + wholenumber + " numerator:" + numerator + " denominator:" + denominator;    	
- }
-    
-    
-    public static String turnImproperFractionToMixedFraction(int numerator , int denominator) {
-        //Integer a = numerator / denominator;
-        //Integer b = numerator % denominator);
-        //return a != 0 ? (a + " " + b + "/" + denominator) : (b + "/" + denominator);
-    	return "";
-    }   
-    
+
+	// ** IMPORTANT ** DO NOT DELETE THIS FUNCTION. This function will be used to
+	// test your code
+	// This function takes a String 'input' and produces the result
+	//
+	// input is a fraction string that needs to be evaluated. For your program, this
+	// will be the user input.
+	// e.g. input ==> "1/2 + 3/4"
+	//
+	// The function should return the result of the fraction after it has been
+	// calculated
+	// e.g. return ==> "1_1/4"
+	public static String produceAnswer(String input) {
+		// Requirement 1
+		// Input will be in the form of a value, followed by an arithmetic operator, and
+		// then another value. Values and operators will be separated by a single space.
+		// Values will contain no spaces.
+		String[] arrayinput = input.split(" ");
+		if (arrayinput.length != 3) {
+			return "Input Expression is not in the correct Format. Values and operators will be separated by a single space";
+		} else {
+			String operand1 = arrayinput[0];
+			String operator = arrayinput[1];
+			String operand2 = arrayinput[2];
+			// Input values may be in the form of
+			// mixed fractions 1_3/4
+			// proper fractions 2/5
+			// improper fractions 8/4
+			// integers. -1
+			// The integer and fraction parts of a mixed fraction will be separated by an
+			// underscore (_) (e.g., “1_3/4” is one and three fourth to distinguish it from
+			// “13/4” which is thirteen fourth).
+			parseFraction(operand1, true);
+			parseFraction(operand2, false);
+			FracCalc.operator = operator;
+			return "4";
+		}
+	}
+
+	// TODO: Fill in the space below with any helper methods that you think you will
+	// need
+
+	public static void parseFraction(String operand, Boolean operand1) {
+		String fraction = operand;
+		String wholenumber = "0";
+		String numerator = "0";
+		String denominator = "0";
+		String[] splitwholenumber = fraction.split("_");
+		if (splitwholenumber.length == 2) {
+			// Mixed Fraction Condition
+			wholenumber = fraction.split("_")[0];
+			numerator = fraction.split("_")[1].split("/")[0];
+			denominator = fraction.split("_")[1].split("/")[1];
+		} else {
+			// Check if it's whole number only
+			String[] splitnumeratoranddenominator = fraction.split("/");
+			// If there are 2 values in array, this means it's a fraction else it's only a
+			// whole number
+			if (splitnumeratoranddenominator.length == 2) {
+				numerator = splitnumeratoranddenominator[0];
+				denominator = splitnumeratoranddenominator[1];
+			} else {
+				wholenumber = operand;
+				denominator = "1";
+			}
+		}
+
+		SetStaticFieldValues(operand1, Integer.parseInt(wholenumber), Integer.parseInt(numerator),
+				Integer.parseInt(denominator));
+
+	}
+
+	public static void SetStaticFieldValues(Boolean operand1, int wholenumber, int numerator, int denominator) {
+
+		if (operand1) {
+			FracCalc.operand1Wholenumber = wholenumber;
+			FracCalc.operand1Numerator = numerator;
+			FracCalc.operand1Denominator = denominator;
+			if (wholenumber != 0) {
+				FracCalc.operand1ImproperNumerator = (wholenumber * denominator) + numerator;
+			} else {
+				FracCalc.operand1ImproperNumerator = numerator;
+			}
+
+		} else {
+			FracCalc.operand2Wholenumber = wholenumber;
+			FracCalc.operand2Numerator = numerator;
+			FracCalc.operand2Denominator = denominator;
+			if (wholenumber != 0) {
+				FracCalc.operand2ImproperNumerator = (wholenumber * denominator) + numerator;
+			} else {
+				FracCalc.operand2ImproperNumerator = numerator;
+			}
+		}
+	}
+
+	public static int findGcd(int number1, int number2) {
+		// In Euclid's algorithm, we start with two numbers X and Y. If Y is zero then
+		// greatest common divisor of both will be X, but if Y is not zero then we
+		// assign the Y to X and Y becomes X%Y. Once again we check if Y is zero, if yes
+		// then we have our greatest common divisor or GCD otherwise we keep continue
+		// like this until Y becomes zero. Since we are using modulo operator, the
+		// number is getting smaller and smaller at each iteration, so the X%Y will
+		// eventually become zero.
+		if (number2 == 0) {
+			return number1;
+		}
+		return findGcd(number2, number1 % number2);
+	}
+
+	public static String Add() {
+		String result = "";
+		int tempnumerator = 0;
+		int commondenominator = 0;
+		if (FracCalc.operand1Denominator == FracCalc.operand2Denominator) {
+			tempnumerator = FracCalc.operand1Numerator + FracCalc.operand2Numerator;
+			if (tempnumerator % FracCalc.operand1Denominator == 0) {
+				result = Integer.toString(tempnumerator / FracCalc.operand1Denominator);
+			} else {
+				result = Integer.toString(tempnumerator) + "/" + Integer.toString(FracCalc.operand1Denominator);
+			}
+		} else {
+			commondenominator = FracCalc.operand1Denominator * FracCalc.operand2Denominator;
+			FracCalc.operand1ImproperNumerator = FracCalc.operand1ImproperNumerator
+					* (commondenominator / FracCalc.operand1Denominator);
+			FracCalc.operand2ImproperNumerator = FracCalc.operand2ImproperNumerator
+					* (commondenominator / FracCalc.operand2Denominator);
+			tempnumerator = FracCalc.operand1ImproperNumerator + FracCalc.operand2ImproperNumerator;
+			if (tempnumerator % commondenominator == 0) {
+				result = Integer.toString(tempnumerator / commondenominator);
+			} else {
+				result = Integer.toString(tempnumerator) + "/" + Integer.toString(commondenominator);
+			}
+
+			int gcd = FracCalc.findGcd(tempnumerator, commondenominator);
+			result = Integer.toString(tempnumerator / gcd) + "/" + Integer.toString(commondenominator / gcd);
+		}
+		return result;
+	}
+
+	public static String parseFractionCheckPoint2(String operand) {
+		String fraction = operand;
+		String wholenumber = "0";
+		String numerator = "0";
+		String denominator = "0";
+		String[] splitwholenumber = fraction.split("_");
+		if (splitwholenumber.length == 2) {
+			// Mixed Fraction Condition
+			wholenumber = fraction.split("_")[0];
+			numerator = fraction.split("_")[1].split("/")[0];
+			denominator = fraction.split("_")[1].split("/")[1];
+		} else {
+			// Check if it's whole number only
+			String[] splitnumeratoranddenominator = fraction.split("/");
+			// If there are 2 values in array, this means it's a fraction else it's only a
+			// whole number
+			if (splitnumeratoranddenominator.length == 2) {
+				numerator = splitnumeratoranddenominator[0];
+				denominator = splitnumeratoranddenominator[1];
+			} else {
+				wholenumber = operand;
+				denominator = "1";
+			}
+		}
+
+		return "whole:" + wholenumber + " numerator:" + numerator + " denominator:" + denominator;
+	}
+
+	public static String turnImproperFractionToMixedFraction(int numerator, int denominator) {
+		// Integer a = numerator / denominator;
+		// Integer b = numerator % denominator);
+		// return a != 0 ? (a + " " + b + "/" + denominator) : (b + "/" + denominator);
+		return "";
+	}
+
 }
